8c5b9fd89c0c53c97520acc236c7f049af372ab5
Mohammed Gufran Ali <mohammedgufranali1212@gmail.com>
2025-01-16 04:24:26 +0530

    greening the grass

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import random
 
 number_of_commits = random.randint(1,5)
 
+print(number_of_commits)
+
 for _ in range(number_of_commits):
     with open('/home/gufran/Desktop/Stalker_Trauma/out.txt','w') as f:
         f.write(str(random.randint(1,100)))

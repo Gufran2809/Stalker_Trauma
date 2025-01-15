2e334de0b5643a128a8c4bad85afa772f442e383
Mohammed Gufran Ali <mohammedgufranali1212@gmail.com>
2025-01-16 04:24:10 +0530

    greening the grass

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import random
 number_of_commits = random.randint(1,5)
 
 for _ in range(number_of_commits):
-    with open('out.txt','w') as f:
+    with open('/home/gufran/Desktop/Stalker_Trauma/out.txt','w') as f:
         f.write(str(random.randint(1,100)))
-
+    os.system("cd /home/gufran/Desktop/Stalker_Trauma")
     os.system("git add .")
     os.system('git commit -m "greening the grass"')
     os.system('git push origin main')

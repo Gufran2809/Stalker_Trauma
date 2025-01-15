542c1fe394eade91d2adc42372dc0d6228ba953b
Mohammed Gufran Ali <mohammedgufranali1212@gmail.com>
2025-01-16 03:49:03 +0530

    greening the grass

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import random
 
 
 with open('out.txt','w') as f:
-    f.write(str(random.randint(1,100)))
     os.system("git add .")
     os.system('git commit -m "greening the grass"')
     os.system('git push origin main')
+    f.write(str(random.randint(1,100)))
 
 
 
